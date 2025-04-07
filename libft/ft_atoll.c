1136c573cb6ec613f7df02fb5f07f04defaf4031
carlos-j <carlos-j@student.42porto.com>
2025-04-07 16:14:21 +0100

    202504071614

---
+++ libft/ft_atoll.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlos-j <carlos-j@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 10:41:30 by carlos-j          #+#    #+#             */
+/*   Updated: 2025/04/07 10:47:00 by carlos-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long long	ft_atoll(const char *nptr)
+{
+	long long	digit;
+	int			sign;
+
+	digit = 0;
+	sign = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == ' ')
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		digit = (digit * 10) + (*nptr - '0');
+		nptr++;
+	}
+	return (digit * sign);
+}
